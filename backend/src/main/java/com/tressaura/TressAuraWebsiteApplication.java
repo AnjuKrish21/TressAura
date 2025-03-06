358de074e716190c311c0d7366d0c0474900258f
Anju Krishnan <anju.krishnan@capgemini.com>
2025-03-06 13:38:43 +0530

    added backend

---
+++ backend/src/main/java/com/tressaura/TressAuraWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.tressaura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TressAuraWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TressAuraWebsiteApplication.class, args);
+	}
+
+}
